d3ca7f988168b083f32c5cf74ad846a563485d29
Andrew Castillo <andrewcast32@gmail.com>
2021-05-12 19:39:10 -0700

    setting up for heroku

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+// heroku works a certain way with port
+// if the port isnt defined, it'll auto to 5000, otherwise it will take the port
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
